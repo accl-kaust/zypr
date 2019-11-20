603b90810e3310ed8e38b2ed799f10e031f5573b
Alex Bucknall <alex.bucknall@gmail.com>
2019-11-20 12:03:33 +0000

    updated start script for dependencies

--- example/start.sh
+++ example/start.sh
@@ -2,13 +2,14 @@
 
 ERROR='\e[0;31m'
 SUCCESS="\e[0;32m"
+WARNING="\e[0;33m"
+INFO="\e[0;34m"
 NONE="\e[0m"
 
 ZYCAP_ROOT_PATH=$(pwd)
 BOARD=$(jq .project.project_device.family global_config.json | tr -d \")
 DESIGN_NAME=$(jq .design.design_name global_config.json | tr -d \")
-CHECK_DEPS=
-
+INSTALL_DEPS=$(jq .config.config_settings.check_dependencies global_config.json | tr -d \")
 VIVADO_PATH=$(jq .config.config_vivado.vivado_path global_config.json | tr -d \")
 VIVADO_PARAMS=$(jq .config.config_vivado.vivado_params global_config.json | tr -d \")
 
@@ -17,10 +18,14 @@ if [ ! -d "$ZYCAP_ROOT_PATH/rtl/.logs" ]; then
     mkdir $ZYCAP_ROOT_PATH/rtl/.logs
 fi
 
+# if [ ${1} == "clean" ]; then
+#     make clean
+# fi
+
 check_error() {
     local error=$( grep "^ERROR" $1 )
     if [ -n "$error" ]; then
-        echo -e "\e[31m$1 - $error\e[39m"
+        echo -e "${ERROR}$1 - $error${NONE}"
     fi
 }
 
@@ -43,9 +48,11 @@ show_spinner()
 install_python_deps()
 {
     if command -v python3 &>/dev/null; then
-        pip3 install -r ../scripts/python/requirements.txt
+        echo -e "${INFO}Installing Python libraries...${NONE}"
+        pip3 install -r ../scripts/python/requirements.txt > "$ZYCAP_ROOT_PATH/rtl/.logs/python_install.log" &
+        show_spinner $!
     else
-        echo -e "\e[31mError - Python 3 is missing.\e[39m"
+        echo -e "${ERROR}Error - Python 3 is missing.${NONE}"
         exit 1
     fi
 }
@@ -53,59 +60,77 @@ install_python_deps()
 install_perl_deps()
 {
     if command -v perl &>/dev/null; then
-        ../scripts/perl/install.sh
+        echo -e "${INFO}Installing Perl libraries...${NONE}"
+        ../scripts/perl/install.sh > "$ZYCAP_ROOT_PATH/rtl/.logs/perl_install.log" &
+        show_spinner $!
     else
-        echo -e "\e[31mError - Perl is missing.\e[39m"
+        echo -e "${ERROR}Error - Perl is missing.${NONE}"
         exit 1
     fi
 }
 
+install_board_files()
+{
+    local BOARD_NAME=$(jq .project.project_device.name ../global_config.json | tr -d \")
+    local BOARD_VER=$(jq .project.project_device.version ../global_config.json | tr -d \")
+
+    cd ../board
+    echo -e "${WARNING}May requires root for adding board files to Xilinx tool paths...${NONE}"
+    sudo bash install.sh ${VIVADO_PATH} ${BOARD_NAME} $BOARD_VER
+    cd ../rtl
+}
+
 echo "Checking Dependencies..."
-if [ $(jq .project.project_device.family global_config.json | tr -d \") == "true" ]; then
+if [ $INSTALL_DEPS == "true" ]; then
+    echo -e "${WARNING}Dependencies not found, install...${NONE}"
+    # install_board_files
     install_python_deps
     install_perl_deps
-    jq '.config.config_settings.check_dependencies=false' global_config.json
+    $(jq -r '.config.config_settings.check_dependencies=false' "$ZYCAP_ROOT_PATH/global_config.json" > tmp.$$.json && mv tmp.$$.json "$ZYCAP_ROOT_PATH/global_config.json")
+    echo -e "${SUCCESS}Installed dependencies \u2713 ${NONE}"
+else
+    echo -e "${SUCCESS}Finished \u2713 ${NONE}"
 fi
 
 echo "Extracting Modules..."
 if [ ! -d "$ZYCAP_ROOT_PATH/rtl/.json" ]; then
-    echo -e "\e[33mNot found, generating...\e[39m"
+    echo -e "${WARNING}Not found, generating...${NONE}"
     perl $ZYCAP_ROOT_PATH/scripts/perl/extract_modules.pl "$ZYCAP_ROOT_PATH/rtl" > "$ZYCAP_ROOT_PATH/rtl/.logs/extract_modules.log"
     check_error "$ZYCAP_ROOT_PATH/rtl/.logs/extract_modules.log"
 fi
-echo -e "\e[32mFinished \u2713 \e[39m"
+echo -e "${SUCCESS}Finished \u2713 ${NONE}"
 
 echo "Generating Black Boxes..."
 if [ ! -d "$ZYCAP_ROOT_PATH/rtl/.blackbox" ]; then
-    echo -e "\e[33mNot found, generating...\e[39m"
+    echo -e "${WARNING}Not found, generating...${NONE}"
     python $ZYCAP_ROOT_PATH/scripts/python/generate_blackbox.py > "$ZYCAP_ROOT_PATH/rtl/.logs/generate_bb.log"
     check_error "$ZYCAP_ROOT_PATH/rtl/.logs/generate_bb.log"
 fi
-echo -e "\e[32mFinished \u2713 \e[39m"
+echo -e "${SUCCESS}Finished \u2713 ${NONE}"
 
 echo "Constructing Configs & Modes..."
 if [ ! -d "$ZYCAP_ROOT_PATH/rtl/.modes" ]; then
-    echo -e "\e[33mNot found, generating...\e[39m"
+    echo -e "${WARNING}Not found, generating...${NONE}"
     python $ZYCAP_ROOT_PATH/scripts/python/generate_interface.py > "$ZYCAP_ROOT_PATH/rtl/.logs/construct_pr.log"
     check_error "$ZYCAP_ROOT_PATH/rtl/.logs/construct_pr.log"
 fi
-echo -e "\e[32mFinished \u2713\e[39m"
+echo -e "${SUCCESS}Finished \u2713${NONE}"
 
 echo "Synthesising PR Configs & Modes..."
 if [ ! -d "$ZYCAP_ROOT_PATH/rtl/.checkpoint_prj" ]; then
-    echo -e "\e[33mNot found, generating...\e[39m"
+    echo -e "${WARNING}Not found, generating...${NONE}"
     exec $ZYCAP_ROOT_PATH/scripts/tcl/synth/synth.sh $VIVADO_PATH $ZYCAP_ROOT_PATH/scripts/tcl/synth/generate_checkpoints.tcl $ZYCAP_ROOT_PATH > "$ZYCAP_ROOT_PATH/rtl/.logs/pr_synth.log" &
     show_spinner $!
     check_error "$ZYCAP_ROOT_PATH/rtl/.logs/pr_synth.log"
 fi
-echo -e "\e[32mFinished \u2713\e[39m"
+echo -e "${SUCCESS}Finished \u2713${NONE}"
 
 echo "Building Block Diagram..."
 if [ ! -d "$ZYCAP_ROOT_PATH/rtl/$DESIGN_NAME" ]; then
-    echo -e "\e[33mNot found, generating...\e[39m"
+    echo -e "${WARNING}Not found, generating...${NONE}"
     exec $VIVADO_PATH -mode batch -source $ZYCAP_ROOT_PATH/scripts/tcl/boards/$BOARD/gen_bd.tcl -tclargs $ZYCAP_ROOT_PATH > "$ZYCAP_ROOT_PATH/rtl/.logs/bd_output.log" &
     show_spinner $!
     check_error "$ZYCAP_ROOT_PATH/rtl/.logs/bd_output.log"
 fi
-echo -e "\e[32mFinished \u2713\e[39m"
+echo -e "${SUCCESS}Finished \u2713${NONE}"
 
