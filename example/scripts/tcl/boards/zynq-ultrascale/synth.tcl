07e8812e2ebadff0ba571d0f67d9355c2bbd07e9
Alex Bucknall <alex.bucknall@gmail.com>
2019-12-09 17:40:38 +0000

    Updated zynqmp synth tcl

--- example/scripts/tcl/boards/zynq-ultrascale/synth.tcl
+++ example/scripts/tcl/boards/zynq-ultrascale/synth.tcl
@@ -6,8 +6,12 @@ set cfg [read [open "$ROOT_PATH/global_config.json" r]]
 set global_config [json::json2dict $cfg] 
 
 source "$ROOT_PATH/scripts/tcl/generic/cpu_threads.tcl"
+source "$ROOT_PATH/scripts/tcl/generic/log_colors.tcl"
 
 open_project "$ROOT_PATH/rtl/base_design/base_design.xpr"
+
+# reset_run synth_1
+
 
 if {[get_property PROGRESS [get_runs synth_1]] != "100%"} {
     set threads [numberOfCPUs]
@@ -36,52 +40,97 @@ close_design
 # P & R (For each mode & config)
 ###################################################
 
+set design_checkpoints {}
+
 dict for {mode modes} [dict get $global_config design design_mode] {
     dict for {configuration configurations} [dict get $global_config design design_mode $mode configs] {
-        open_checkpoint "$ROOT_PATH/rtl/base_design/base_design.checkpoints/base.dcp"
 
-        # update_design -cell base_design_i/partial_led_test_v1_0_0/inst/partial_led_test_v1_0_S00_AXI_inst -black_box
-
-        set_property HD.RECONFIGURABLE true [get_cells base_design_i/partial_led_test_v1_0_0/inst/partial_led_test_v1_0_S00_AXI_inst]
-
-        read_checkpoint "$ROOT_PATH/rtl/.modes/$mode/$configuration/.checkpoints/pr_module.dcp" -cell "base_design_i/partial_led_test_v1_0_0/inst/partial_led_test_v1_0_S00_AXI_inst"
-
-        if {[get_pblocks] eq ""} {
-            startgroup
-            create_pblock pblock_1
-            resize_pblock pblock_1 -add CLOCKREGION_X0Y1:CLOCKREGION_X0Y1
-            add_cells_to_pblock pblock_1 [get_cells [list base_design_i/partial_led_test_v1_0_0/inst/partial_led_test_v1_0_S00_AXI_inst]] -clear_locs
-            endgroup
+        # puts "$INFO Generating for $mode $configuration... $NONE"
+        if { ![info exists initial_checkpoint] } {
+            set initial_checkpoint "$ROOT_PATH/rtl/base_design/base_design.checkpoints/$mode-$configuration.dcp"
         }
 
-        file mkdir "$ROOT_PATH/rtl/.modes/$mode/$configuration/.constraint"
-        write_xdc -force "$ROOT_PATH/rtl/.modes/$mode/$configuration/.constraint/top.xdc"
-        
-        opt_design > $log_dir/vivado_cp_$mode-$configuration-opt.log
-        place_design > $log_dir/vivado_cp_$mode-$configuration-place.log
-        route_design > $log_dir/vivado_cp_$mode-$configuration-route.log
+        if { ![file exists "$ROOT_PATH/rtl/base_design/base_design.checkpoints/static.dcp"] } {
 
-        # write_checkpoint $ROOT_PATH/rtl/.modes/$mode/$configuration/.checkpoints/pr_module.dcp -force
-        write_checkpoint -force "$ROOT_PATH/rtl/base_design/base_design.checkpoints/$mode-$configuration.dcp"
-        close_design
+            puts "${WARNING}No static logic exists, generating... $NONE"
+
+            open_checkpoint "$ROOT_PATH/rtl/base_design/base_design.checkpoints/base.dcp"
+
+            # update_design -cell base_design_i/partial_led_test_v1_0_0/inst/partial_led_test_v1_0_S00_AXI_inst -black_box
+
+            set_property HD.RECONFIGURABLE true [get_cells base_design_i/partial_led_test_v1_0_0/inst/partial_led_test_v1_0_S00_AXI_inst]
+
+            read_checkpoint "$ROOT_PATH/rtl/.modes/$mode/$configuration/.checkpoints/pr_module.dcp" -cell "base_design_i/partial_led_test_v1_0_0/inst/partial_led_test_v1_0_S00_AXI_inst"
+
+            if {[get_pblocks] eq ""} {
+                startgroup
+                create_pblock pblock_1
+                # Could use Vipin's work to generate this
+                resize_pblock pblock_1 -add CLOCKREGION_X0Y1:CLOCKREGION_X0Y1
+                add_cells_to_pblock pblock_1 [get_cells [list base_design_i/partial_led_test_v1_0_0/inst/partial_led_test_v1_0_S00_AXI_inst]] -clear_locs
+                endgroup
+            }
+
+            file mkdir "$ROOT_PATH/rtl/.modes/$mode/$configuration/.constraint"
+            write_xdc -force "$ROOT_PATH/rtl/.modes/$mode/$configuration/.constraint/top.xdc"
+
+            puts "${INFO}Generating $mode $configuration checkpoint... $NONE"
+
+            opt_design > $log_dir/vivado_cp_$mode-$configuration-opt.log
+            place_design > $log_dir/vivado_cp_$mode-$configuration-place.log
+            route_design > $log_dir/vivado_cp_$mode-$configuration-route.log
+
+            write_checkpoint -force "$ROOT_PATH/rtl/base_design/base_design.checkpoints/$mode-$configuration.dcp"
+
+            # Generate static logic
+
+            update_design -cell base_design_i/partial_led_test_v1_0_0/inst/partial_led_test_v1_0_S00_AXI_inst -black_box
+
+            lock_design -level routing
+
+            puts "${INFO}Generating static checkpoint... $NONE"
+
+            write_checkpoint -force "$ROOT_PATH/rtl/base_design/base_design.checkpoints/static.dcp"
+
+        } else {
+
+            puts "${INFO}Generating $mode $configuration checkpoint... $NONE"
+
+            open_checkpoint "$ROOT_PATH/rtl/base_design/base_design.checkpoints/static.dcp"
+
+            read_checkpoint "$ROOT_PATH/rtl/.modes/$mode/$configuration/.checkpoints/pr_module.dcp" -cell "base_design_i/partial_led_test_v1_0_0/inst/partial_led_test_v1_0_S00_AXI_inst"
+
+            # file mkdir "$ROOT_PATH/rtl/.modes/$mode/$configuration/.constraint"
+            # write_xdc -force "$ROOT_PATH/rtl/.modes/$mode/$configuration/.constraint/top.xdc"
+            
+            opt_design > $log_dir/vivado_cp_$mode-$configuration-opt.log
+            place_design > $log_dir/vivado_cp_$mode-$configuration-place.log
+            route_design > $log_dir/vivado_cp_$mode-$configuration-route.log
+
+            # write_checkpoint $ROOT_PATH/rtl/.modes/$mode/$configuration/.checkpoints/pr_module.dcp -force
+            write_checkpoint -force "$ROOT_PATH/rtl/base_design/base_design.checkpoints/$mode-$configuration.dcp"
+
+            lappend design_checkpoints "$ROOT_PATH/rtl/base_design/base_design.checkpoints/$mode-$configuration.dcp"
+
+        }
+
+        if {[current_design] ne ""} {
+            close_design
+        }
+
     }
 }
 
+puts "$INFO[pr_verify -initial $initial_checkpoint -additional $design_checkpoints]$NONE"
 
-# update_design -cell base_design_i/partial_led_test_v1_0_0/inst/partial_led_test_v1_0_S00_AXI_inst -black_box
+file mkdir "$ROOT_PATH/rtl/base_design/base_design.bitstreams"
 
-# # link_design > $log_dir/vivado_bd_link.log
-# opt_design > $log_dir/vivado_cp_SPECIFIC_opt.log
-# place_design > $log_dir/vivado_cp_SPECIFIC_place.log
-# route_design > $log_dir/vivado_cp_SPECIFIC_route.log
-
-# write_checkpoint -force "$ROOT_PATH/rtl/base_design/base_design.checkpoints/mode_a_config_a.dcp"
-
-# # generate static region
-# update_design -cell base_design_i/partial_led_test_v1_0_0/inst/partial_led_test_v1_0_S00_AXI_inst -black_box
-
-# lock_design -level routing
-
-# write_checkpoint -force "$ROOT_PATH/rtl/base_design/base_design.checkpoints/static.dcp"
-
-close_design
+dict for {mode modes} [dict get $global_config design design_mode] {
+    dict for {configuration configurations} [dict get $global_config design design_mode $mode configs] {
+        open_checkpoint "$ROOT_PATH/rtl/base_design/base_design.checkpoints/$mode-$configuration.dcp"
+        write_bitstream -force "$ROOT_PATH/rtl/base_design/base_design.bitstreams/$mode-$configuration.bit"
+        if {[current_design] ne ""} {
+            close_design
+        }
+    }
+}
